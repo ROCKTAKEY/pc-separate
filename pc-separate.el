97f98cc55c272c8259e7352241384807f3553155
ROCKTAKEY <rocktakey@gmail.com>
2019-01-08 10:16:10 +0900

    Reneme package prefix to "separate".

--- pc-separate.el
+++ pc-separate.el
@@ -23,55 +23,55 @@
 ;;; Commentary:
 
 ;; * Change action by pc system
-;;   "pc-separate" provide function that help you to separate setting
+;;   "separate" provide function that help you to separate setting
 ;;   by system name given by "(system-name)". 
 ;; * Words
 ;;   -  "system" means system-name or symbol which is defined in
-;;      `pc-separate-system-alist'.
+;;      `separate-system-alist'.
 ;; * Variables
-;; ** `pc-separate-system-alist'
+;; ** `separate-system-alist'
 ;;    an associated list. Each element is cons cell,
 ;;    "(system-name . number-or-symbol)". in this package, you can use
 ;;    "number-or-symbol" as "system" instead of "system-name".
 ;; * Macros
-;; ** "`pc-separate-set' (variable alist)
+;; ** "`separate-set' (variable alist)
 ;;   - Set value of "VARIABLE" depend on "system".
 ;;   - Each element of "ALIST" is "(SYSTEM . VALUE)", and "VARIABLE" is
 ;;     set to "VALUE" if "SYSTEM" accords current system.
 ;;   - If there are some cons cells whose car accords current system,
-;;     "number-or-symbol" defined on upper stream in "pc-separate-system-alist"
+;;     "number-or-symbol" defined on upper stream in "separate-system-alist"
 ;;     is used. System-name is the lowest priority.
 ;;   - in the cons cell whose "SYSTEM" is "default", its "VALUE" is used only
 ;;     when any other "SYSTEM" doesn't accord current system.
-;; ** `pc-separate-setq' (variable alist)
-;;    Same as pc-separate-set, but "VARIABLE" doesn't have to be quoted.
+;; ** `separate-setq' (variable alist)
+;;    Same as separate-set, but "VARIABLE" doesn't have to be quoted.
 
 
 ;;; Code:
 
 (require 'cl-lib)
 
-(defgroup pc-separate nil
-  "pc-separate group."
+(defgroup separate nil
+  "separate group."
   :group 'tools)
 
-(defcustom pc-separate-system-alist '()
-  "Relationship of \"number or symbol\", and system-name on pc-separate.
+(defcustom separate-system-alist '()
+  "Relationship of \"number or symbol\", and system-name on separate.
 You can add (system-name . number-or-symbol).
-And you can use the number instead of system-name on pc-separate."
-  :group 'pc-separate
+And you can use the number instead of system-name on separate."
+  :group 'separate
   :type '(set (cons string (choice integer symbol)))
   )
 
 
 
-(defun pc-separate--cdr-assoc-all (key alist &optional func)
+(defun separate--cdr-assoc-all (key alist &optional func)
   ""
   (cl-loop for (k . v) in alist
            if (funcall (if func func 'equal) key k)
            collect v))
 
-(defun pc-separate--assoc-or-eval (key alist)
+(defun separate--assoc-or-eval (key alist)
   "Same as assq, but if car of element of ALIST is list, and eval it.
 If the value is t, treat the element as matched."
   (cl-loop
@@ -88,35 +88,35 @@ If the value is t, treat the element as matched."
    return y)
   )
 
-(defun pc-separate--system-to-system-name (system)
+(defun separate--system-to-system-name (system)
   "Return system-name which SYSTEM is representing."
   (let
       ((sys-name (if (stringp system)
                      system
                    ;; if no system-name, return nil
-                   (car (rassq system pc-separate-system-alist)))))
+                   (car (rassq system separate-system-alist)))))
     (when sys-name
       sys-name)))
 
-(defun pc-separate--system-name-to-system (sys-name)
+(defun separate--system-name-to-system (sys-name)
   "Return list whose all element is representing SYS-NAME."
-  (let* ((p (pc-separate--cdr-assoc-all sys-name pc-separate-system-alist))
+  (let* ((p (separate--cdr-assoc-all sys-name separate-system-alist))
          (system-list (if p
                           (append p (list sys-name))
                         (list sys-name))))
     system-list))
 
-(defun pc-separate--current-system-p (system)
+(defun separate--current-system-p (system)
   "Return non-nil if SYSTEM is representing current system."
-  (string= (pc-separate--system-to-system-name system) (system-name)))
+  (string= (separate--system-to-system-name system) (system-name)))
 
 
 
 ;;;###autoload
-(defmacro pc-separate-set (variable alist)
+(defmacro separate-set (variable alist)
   "Set value of VARIABLE each system.
 each element of ALIST is (SYSTEM . VALUE), and VARIABLE is set to VALUE
-if (pc-separate-current-system-p SYSTEM) return non-nil."
+if (separate-current-system-p SYSTEM) return non-nil."
   (let* ((candicate (gensym "condicate"))
          (result (gensym "result"))
          (system-list (gensym "system-list"))
@@ -129,7 +129,7 @@ if (pc-separate-current-system-p SYSTEM) return non-nil."
              (cl-loop
               with ,result = nil
               with ,system-list =
-              (pc-separate--system-name-to-system (system-name))
+              (separate--system-name-to-system (system-name))
               for ,s in (append ,system-list (list 'default))
               ;; assq throw error if ALIST is NOT list.
               do (setq ,result (assoc ,s ,alst)) 
@@ -143,21 +143,21 @@ if (pc-separate-current-system-p SYSTEM) return non-nil."
        ,candicate)))
 
 ;;;###autoload
-(defmacro pc-separate-setq (variable alist)
+(defmacro separate-setq (variable alist)
   "Set value of VARIABLE each system.
 each element of ALIST is (SYSTEM . VALUE), and VARIABLE is set to VALUE
-if (pc-separate-current-system-p SYSTEM) return non-nil.
+if (separate-current-system-p SYSTEM) return non-nil.
 variable have to be non-quoted."
-  `(pc-separate-set (quote ,variable) ,alist))
+  `(separate-set (quote ,variable) ,alist))
 
 ;;;###autoload
-(defmacro pc-separate-cond (clauses)
+(defmacro separate-cond (clauses)
   "Eval BODY if SYSTEM accords current system.
 Each clause looks like (SYSTEM BODY...).BODY is evaluate
-if (pc-separate-current-system-p SYSTEM) return non-nil."
+if (separate-current-system-p SYSTEM) return non-nil."
   (let ((c (gensym)))
     `(let (,c)
-       (pc-separate-setq ,c ',clauses)
+       (separate-setq ,c ',clauses)
        (eval (cons 'progn ,c)))))
 
 (provide 'pc-separate)
