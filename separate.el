6b0aca8015bee76f8128804c1f987e8532ad2728
ROCKTAKEY <rocktakey@gmail.com>
2019-01-23 21:05:20 +0900

    Change the word "system" to "separator" in *.el files.

--- separate.el
+++ separate.el
@@ -26,23 +26,23 @@
 ;;   "separate" provide function that help you to separate setting
 ;;   by system name given by "(system-name)". 
 ;; * Words
-;;   -  "system" means system-name or symbol which is defined in
+;;   -  "separator" means system-name or symbol which is defined in
 ;;      `separate-system-alist'.
 ;; * Variables
 ;; ** `separate-system-alist'
 ;;    an associated list. Each element is cons cell,
 ;;    "(system-name . number-or-symbol)". in this package, you can use
-;;    "number-or-symbol" as "system" instead of "system-name".
+;;    "number-or-symbol" as "separator" instead of "system-name".
 ;; * Macros
 ;; ** "`separate-set' (variable alist)
-;;   - Set value of "VARIABLE" depend on "system".
-;;   - Each element of "ALIST" is "(SYSTEM . VALUE)", and "VARIABLE" is
-;;     set to "VALUE" if "SYSTEM" accords current system.
+;;   - Set value of "VARIABLE" depend on "SEPARATOR".
+;;   - Each element of "ALIST" is "(SEPARATOR . VALUE)", and "VARIABLE" is
+;;     set to "VALUE" if "SEPARATOR" accords current system.
 ;;   - If there are some cons cells whose car accords current system,
 ;;     "number-or-symbol" defined on upper stream in "separate-system-alist"
 ;;     is used. System-name is the lowest priority.
-;;   - in the cons cell whose "SYSTEM" is "default", its "VALUE" is used only
-;;     when any other "SYSTEM" doesn't accord current system.
+;;   - in the cons cell whose "SEPARATOR" is "default", its "VALUE" is used only
+;;     when any other "SEPARATOR" doesn't accord current system.
 ;; ** `separate-setq' (variable alist)
 ;;    Same as separate-set, but "VARIABLE" doesn't have to be quoted.
 
@@ -55,7 +55,7 @@
   "separate group."
   :group 'tools)
 
-(defcustom separate-system-alist '()
+(defcustom separate-separator-alist '()
   "Relationship of \"number or symbol\", and system-name on separate.
 You can add (system-name . number-or-symbol).
 And you can use the number instead of system-name on separate."
@@ -72,7 +72,7 @@ And you can use the number instead of system-name on separate."
            collect v))
 
 (defun separate--assoc-or-eval (key alist)
-  "Same as assq, but if car of element of ALIST is list, and eval it.
+  "Same as assoc, but if car of element of ALIST is list, and eval it.
 If the value is t, treat the element as matched."
   (cl-loop
    with candicate = nil
@@ -88,37 +88,37 @@ If the value is t, treat the element as matched."
    return y)
   )
 
-(defun separate--system-to-system-name (system)
-  "Return system-name which SYSTEM is representing."
+(defun separate--separator-to-system-name (separator)
+  "Return system-name which SEPARATOR is representing."
   (let
-      ((sys-name (if (stringp system)
-                     system
+      ((sys-name (if (stringp separator)
+                     separator
                    ;; if no system-name, return nil
-                   (car (rassq system separate-system-alist)))))
+                   (car (rassq separator separate-separator-alist)))))
     (when sys-name
       sys-name)))
 
-(defun separate--system-name-to-system (sys-name)
-  "Return list whose all element is representing SYS-NAME."
-  (let* ((p (separate--cdr-assoc-all sys-name separate-system-alist))
-         (system-list (if p
-                          (append p (list sys-name))
-                        (list sys-name))))
-    system-list))
+(defun separate--system-name-to-separators (system-name)
+  "Return list whose all element is representing SYSTEM-NAME."
+  (let* ((p (separate--cdr-assoc-all system-name separate-separator-alist))
+         (separator-list (if p
+                             (append p (list system-name))
+                           (list system-name))))
+    separator-list))
 
-(defun separate--current-system-p (system)
-  "Return non-nil if SYSTEM is representing current system."
-  (string= (separate--system-to-system-name system) (system-name)))
+(defun separate--current-separator-p (separator)
+  "Return non-nil if SEPARATOR is representing current system."
+  (string= (separate--separator-to-system-name separator) (system-name)))
 
 
 
 ;;;###autoload
 (defmacro separate-set (variable alist)
   "Set value of VARIABLE each system.
-each element of ALIST is (SYSTEM . VALUE), and VARIABLE is set to VALUE
-if (separate-current-system-p SYSTEM) return non-nil."
+each element of ALIST is (SEPARATOR . VALUE), and VARIABLE is set to VALUE
+if (separate-current-separator-p SEPARATOR) return non-nil."
   (let ((valid-cons (gensym "valid-cons"))
-        (candicate (gensym "candicate"))
+        (separator (gensym "separator"))
         (value (gensym "value"))
         (alst alist))
     ;; throw error if ALIST is NOT both alist and symbol.
@@ -126,9 +126,9 @@ if (separate-current-system-p SYSTEM) return non-nil."
     `(let (,valid-cons)
        (setq ,valid-cons
              (cl-loop
-              for (,candicate . ,value) in ,alst
-              if (separate--current-system-p ,candicate)
-              return (cons ,candicate ,value)
+              for (,separator . ,value) in ,alst
+              if (separate--current-separator-p ,separator)
+              return (cons ,separator ,value)
               end
               finally return nil   ;if never eval "return form" return nil
               ))
@@ -139,16 +139,16 @@ if (separate-current-system-p SYSTEM) return non-nil."
 ;;;###autoload
 (defmacro separate-setq (variable alist)
   "Set value of VARIABLE each system.
-each element of ALIST is (SYSTEM . VALUE), and VARIABLE is set to VALUE
-if (separate-current-system-p SYSTEM) return non-nil.
+each element of ALIST is (SEPARATOR . VALUE), and VARIABLE is set to VALUE
+if (separate-current-SEPARATOR-p SEPARATOR) return non-nil.
 variable have to be non-quoted."
   `(separate-set (quote ,variable) ,alist))
 
 ;;;###autoload
 (defmacro separate-cond (clauses)
-  "Eval BODY if SYSTEM accords current system.
-Each clause looks like (SYSTEM BODY...).BODY is evaluate
-if (separate-current-system-p SYSTEM) return non-nil."
+  "Eval BODY if SEPARATOR accords current system.
+Each clause looks like (SEPARATOR BODY...). BODY is evaluate
+if (separate-current-separator-p SEPARATOR) return non-nil."
   (let ((c (gensym)))
     `(let (,c)
        (separate-setq ,c ',clauses)
